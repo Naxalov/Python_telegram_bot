598d88ca345a440f691d81306aa753a6d46900b5
ZARIFMAC <uzbzarif@gmail.com>
2021-03-27 19:40:14 +0900

    sendMessage

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 import telegram
 TOKEN = '1602686596:AAGByzpMndN5gTZyPX-0Md5TNl21-pZ5pdc'
 bot = telegram.Bot(token=TOKEN)
-user_bot = bot.getMe()
-print(user_bot.id)
-print(user_bot.first_name)
-print(type(user_bot))
 
+
+bot.send_message(chat_id=86775091,text='Hi')
